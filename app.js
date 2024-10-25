38015b39470d3df194ef6ddb27b377260434d0c5
SENA <SENA@ARMSCCCCTGOD198.FormacionSena.RED>
2024-10-25 11:12:28 -0500

    creation of the 20 products

---
+++ app.js
@@ -0,0 +1,157 @@
+const products = [
+    {
+      id:101,
+      brand: 'Rolex',
+      category:'Lujo',
+      price:'$5,000',
+      img: '/img/505.png',
+      
+    },
+    {
+        id:102,
+        brand: 'Omega ',
+        category:'Lujo',
+        price:'$20,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:103,
+        brand: 'Cartier',
+        category:'Lujo',
+        price:'$30,000 ',
+        img: '/img/505.png',
+    },
+    {
+        id:104,
+        brand: 'Patek Philippe',
+        category:'Lujo',
+        price:'$100,000 ',
+        img: '/img/505.png',
+    },
+    {
+        id:105,
+        brand: 'Audemars Piguet',
+        category:'Lujo',
+        price:'5,000',
+        img: '/img/505.png',
+    },
+    {
+        id:106,
+        brand: 'Breitling',
+        category:'Deportivo',
+        price:'$15,000 ',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:107,
+        brand: 'TAG Heuer',
+        category:'Deportivo',
+        price:'$10,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:108,
+        brand: 'Tissot',
+        category:'Clásico',
+        price:'$3,000',
+        img: '/img/505.png',
+    },
+    {
+        id:109,
+        brand: 'Longines ',
+        category:'Clásico',
+        price:'$5,000 ',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1010,
+        brand: 'Rado ',
+        category:'Clásico',
+        price:'$5,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1011,
+        brand: 'Swatch',
+        category:'Accesible',
+        price:'$500',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1012,
+        brand: 'Fossil',
+        category:'Accesible',
+        price:'$500 ',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1013,
+        brand: 'Skagen',
+        category:'Minimalista',
+        price:'$500 ',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1014,
+        brand: 'Daniel Wellington',
+        category:'Accesible',
+        price:'$200',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1015,
+        brand: 'Movado',
+        category:'Clásico',
+        price:'$1,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1016,
+        brand: 'Vacheron Constantin',
+        category:'Lujo',
+        price:'$100,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1017,
+        brand: 'Jaeger-LeCoultre',
+        category:'Lujo',
+        price:'$5,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1018,
+        brand: 'IWC',
+        category:'Lujo',
+        price:'$20,000',
+        img: '/img/505.png',
+    },
+    {
+        id:1019,
+        brand: 'Richard Mille',
+        category:'Lujo',
+        price:'$60,000',
+        img: '/img/505.png',
+        
+    },
+    {
+        id:1020,
+        brand: 'Breguet',
+        category:'Lujo',
+        price:'$100,000',
+        img: '/img/505.png',
+        
+    },
+  ];
